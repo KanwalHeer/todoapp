277e9107a999c54be74d0b2a2f1b9ee319e185d4
Kanwal <alikanwal382@gmail.com>
2024-05-21 01:26:23 -0700

    addition

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
-import { FaTrash } from "react-icons/fa";
-import { FaEdit } from "react-icons/fa";
+import { FaStar, FaTrash, FaEdit } from "react-icons/fa";
 
 //edit value types
 type editModelTyoe = {
@@ -14,6 +13,7 @@ function Todo() {
   let [todo, settodo] = useState<string>("");
   const [todosList, settodoList] = useState<string[]>([]);
   const [editModel, setEditModel] = useState<boolean>(false);
+  const [impsList, setimpList] = useState<string[]>([]);
   const [editValue, setEditValue] = useState<editModelTyoe>({
     value: "",
     index: null,
@@ -22,26 +22,40 @@ function Todo() {
   //functions
   //addtodo funcction:
   const AddTodo = () => {
+    if (todo.trim() === "") return; //// Check if the input is empty
     settodoList([...todosList, todo]);
-  };
-
-  // clear function;
-  const ClearTodo = () => {
     settodo("");
   };
 
   //delete function;
   const deleteTodo = (index: number) => {
-    settodoList(todosList.filter((item, i) => i !== index));
+    const deletetodolist = todosList.filter((item1, i) => i !== index);
+    settodoList(deletetodolist);
+    setimpList(impsList.filter((item2) => item2 !== todosList[index]));
   };
 
   //edit function;
   const editTodo = (index: number) => {
+    if (todo.trim() === "") return;
     const newTodos = [...todosList];
-    newTodos.splice(index, 1, todo);
+    newTodos[index] = todo;
     settodoList(newTodos);
+    //important todo list
+    if (impsList.includes(editValue.value)) {
+      const newImpsList = impsList.map((item) =>
+        item === editValue.value ? todo : item
+      );
+      setimpList(newImpsList);
+    }
     setEditModel(false);
     settodo("");
+  };
+
+  //makeImportant function
+  const makeImportant = (index: number) => {
+    if (!impsList.includes(todosList[index])) {
+      setimpList([...impsList, todosList[index]]);
+    }
   };
 
   return (
@@ -74,13 +88,6 @@ function Todo() {
           >
             {editModel ? "Edit Todo" : "Add Todo"}
           </button>
-
-          <button
-            className="text-xl shadow-md bg-blue-600 text-white hover:bg-blue-500 rounded p-3"
-            onClick={ClearTodo}
-          >
-            Clear
-          </button>
         </div>
       </div>
 
@@ -110,11 +117,55 @@ function Todo() {
                     setEditValue({ value: mytodo, index: i });
                   }}
                 />
+                <FaStar
+                  className="hover:text-yellow-500 cursor-pointer text-white text-2xl"
+                  onClick={() => makeImportant(i)}
+                />
               </div>
             </div>
           );
         })}
       </div>
+
+      {/* importantlist */}
+      {impsList.length > 0 && (
+        <>
+          <div>
+            <h1 className="text-3xl text-gray-500 font-bold underline text-center mt-4">
+              Important Todo List
+            </h1>
+          </div>
+
+          <div className="w-[10rem] sm:w-[20rem] md:w-[40rem] lg:w-[45rem] xl:w-[50rem]  m-auto flex flex-col gap-2 sm col ">
+            {impsList.map((imptodo: string, i: number) => {
+              return (
+                <div
+                  key={i}
+                  className="bg-violet-600 flex justify-between items-center p-2 rounded-lg shadow-md"
+                >
+                  <div className="flex gap-2">
+                    <div className="text-lg text-white">{imptodo}</div>
+                  </div>
+                  <div className="flex gap-2 min-sm:flex-col">
+                    <FaTrash
+                      className="hover:text-red-500 cursor-pointer text-white text-2xl mr-2"
+                      onClick={() => {
+                        const deleteImpsList = impsList.filter(
+                          (item, j) => j !== i
+                        );
+                        setimpList(deleteImpsList);
+                      }}
+                    />
+                    <FaStar className="hover:text-yellow-700 cursor-pointer text-yellow-500 text-2xl" />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* end implist */}
+        </>
+      )}
     </div>
   );
 }
